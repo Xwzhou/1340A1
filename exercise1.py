087279beeb2b48aa28ce63abf71990ae2ce3b816
michaelnkim <michaeln.kim@mail.utoronto.ca>
2014-09-26 18:22:08 -0400

    Exercise 1 working

--- exercise1.py
+++ exercise1.py
@@ -46,7 +46,8 @@ def grade_to_gpa(grade):
     if type(grade) is str:
         print ("letter") # remove this line once the code is implemented
         # check that the grade is one of the accepted values
-        # assign grade to letter_grade
+
+                # assign grade to letter_grade
     elif type(grade) is int:
         print("mark") # remove this line once the code is implemented
         # check that grade is in the accepted range
@@ -58,9 +59,22 @@ def grade_to_gpa(grade):
         raise TypeError("Invalid type passed as parameter")
 
     # write a long if-statement to convert letter_grade
-    # assign the value to gpa
+    if letter_grade == "A+":
+        gpa = 4.0
     if letter_grade == "A":
         gpa = 4.0
+    if letter_grade == "A-":
+        gpa = 3.7
+    if letter_grade == "B+":
+        gpa = 3.3
+    if letter_grade == "B":
+        gpa = 3.0
+    if letter_grade == "B-":
+        gpa = 2.7
+    if letter_grade == "FZ":
+        gpa = 0.0
+   # assign the value to gpa
 
     return gpa
 
+grade_to_gpa("A")
