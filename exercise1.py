21c0efd4b2827fe0ecff1c5f78ad0cafe2df7f67
Xwzhou <Xw.zhou@mail.utoronto.ca>
2014-10-06 18:29:58 -0400

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -27,67 +27,66 @@ def grade_to_gpa(grade):
         ValueError if parameter is out of range
     """
 
-    gpa=0.0
-    letter_range=["A+","A","A-","B+","B","B-","FZ"]
-    letter_grade=""
+    gpa = 0.0
+    letter_range = ["A+","A","A-","B+","B","B-","FZ"]
+    letter_grade = ""
 
     if type(grade) is str:
         print ("letter") # remove this line once the code is implemented
         if grade in letter_range:
-            else:
-            print ("Invalid type passed as parameter")# check that the grade is one of the accepted values
-        # assign grade to letter_grade
-        elif type(grade) is int:
-            print("mark") # remove this line once the code is implemented
-
-            def mark_to_letter(grade):
-                 # assign the value to letter_grade
-                 # hint: letter_grade = mark_to_letter(grade)
-                mark_range=range(0,101,1)
-                if grade in mark_range:
-                    if grade in mark_range[91:102]:
-                    letter_grade="A+"
-                    if grade in mark_range[86:91]:
-                    letter_grade="A"
-                    if grade in mark_range[81:86]:
-                    letter_grade="A-"
-                    if grade in mark_range[78:81]:
-                    letter_grade="B+"
-                    if grade in mark_range[74:78]:
-                    letter_grade="B"
-                    if grade in mark_range[71:74]:
-                    letter_grade="B-"
-                    if grade in mark_range[0:71]:
-                    letter_grade="FZ"
-
-
+            print ("yes")
         else:
-            print("Invalid type passed as parameter")
-            return  letter_grade
+            print ("Invalid type passed as parameter1")# check that the grade is one of the accepted values
+        # assign grade to letter_grade
+    elif type(grade) is int:
+        print("mark") # remove this line once the code is implemented
+
+    mark_range=range(0, 101, 1)
+
+    def mark_to_letter(grade):
+        # assign the value to letter_grade
+        # hint: letter_grade = mark_to_letter(grade)
         # check that grade is in the accepted range
+
+        if grade in mark_range:
+            print("grade in mark range")
         # convert the numeric grade to a letter grade
+        if grade in mark_range[91:102]:
+            return "A+"
+        if grade in mark_range[86:91]:
+            return "A"
+        if grade in mark_range[81:86]:
+            return "A-"
+        if grade in mark_range[78:81]:
+            return "B+"
+        if grade in mark_range[74:78]:
+            return "B"
+        if grade in mark_range[71:74]:
+            return "B-"
+        if grade in mark_range[0:71]:
+            return "FZ"
+        else: raise ValueError("the mark is out of range")
 
-         gpa = 0.0
-
-     if letter_grade == "A":
-         gpa = 4.0
-     elif letter_grade=="A+":
-         gpa=4.0
-     elif letter_grade=="A-":
-         gpa=3.7
-     elif letter_grade=="B+":
-         gpa=3.3
-     elif letter_grade=="B":
-         gpa=3.0
-     elif letter_grade=="B-":
-         gpa=2.7
-     elif letter_grade=="FZ":
-         gpa=0.0
-    else:
-        # raise a TypeError exception
-        raise TypeError("Invalid type passed as parameter")
-
+    letter_grade = mark_to_letter(grade)
+    print(letter_grade)
+    gpa = 0.0
     # write a long if-statement to convert letter_grade
     # assign the value to gpa
-
-    return gpa
+    if letter_grade == "A+":
+        gpa = 4.0
+    elif letter_grade=="A":
+        gpa = 4.0
+    elif letter_grade=="A-":
+        gpa = 3.7
+    elif letter_grade=="B+":
+        gpa = 3.3
+    elif letter_grade=="B":
+        gpa = 3.0
+    elif letter_grade=="B-":
+        gpa = 2.7
+    elif letter_grade=="FZ":
+        gpa = 0.0
+    else:
+        # raise a TypeError exception
+        raise TypeError("Invalid type passed as parameter3")
+    print(gpa)
