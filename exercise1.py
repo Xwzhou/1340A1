cc1f2e4f6b3fbf38ef210709bcb073f1321cf460
michaelnkim <michaeln.kim@mail.utoronto.ca>
2014-10-06 23:41:57 -0400

    Final

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,126 @@
-git reset --hard HEAD
+#!/usr/bin/env python3
+
+""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+
+This module contains one function grade_to_gpa. It can be passed a parameter
+that is an integer (0-100) or a letter grade (A+, A, A-, B+, B, B-, or FZ). All
+other inputs will result in an error.
+
+Example:
+    $ python exercise1.py
+
+"""
+
+__author__ = 'Susan Sim'
+__email__ = "ses@drsusansim.org"
+
+__copyright__ = "2014 Susan Sim"
+__license__ = "MIT License"
+
+__status__ = "Prototype"
+
+# imports one per line
+
+
+def grade_to_gpa(grade):
+    """
+    Returns the UofT Graduate GPA for a given grade.
+
+    :param:
+        grade (integer or string): Grade to be converted
+            If integer, accepted values are 0-100.
+            If string, accepted values are A+, A, A-, B+, B, B-, FZ
+
+    :return:
+        float: The equivalent GPA
+            Value is 0.0-4.0
+
+    :raises:
+        TypeError if parameter is not a string or integer
+        ValueError if parameter is out of range
+    """
+
+    letter_grade = ""
+    gpa = 0.0
+
+    if type(grade) is str:
+        print ("letter")
+
+        # check that the grade is one of the accepted values
+        def check_grade_string(str_grade):
+            '''
+            :param str_grade(string): input value to be compared with accepted values
+            :return: Value or error
+            '''
+            accepted_values = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'FZ']
+            if str_grade in accepted_values:
+                print("Letter grade is ", str_grade)
+            else:
+               raise ValueError("Letter grade is not one of the accepted values")
+        # assign grade to letter_grade
+        check_grade_string(grade)
+        letter_grade = grade
+
+    elif type(grade) is int:
+        print("mark") # remove this line once the code is implemented
+        # check that grade is in the accepted range
+        def check_grade_integer(int_grade):
+            '''
+            :param int_grade(Integer): input value to be compared with accepted values
+            :return: Print input value or error
+            '''
+            accepted_values = range(0, 101)
+            if int_grade in accepted_values:
+                print("Integer grade is ", int_grade)
+            else:
+                raise ValueError("Integer grade is not one of the accepted values")
+        check_grade_integer(grade)
+
+        # convert the numeric grade to a letter grade
+        def mark_to_letter(int_grade):
+            '''
+            :param int_grade(integer):
+            :return: Str value. Converted letter grade from integer grade.
+            '''
+            if int_grade in range(90,101):
+                return "A+"
+            elif int_grade in range(85,90):
+                return "A"
+            elif int_grade in range(80,85):
+                return "A-"
+            elif int_grade in range(77,80):
+                return "B+"
+            elif int_grade in range(73,77):
+                return "B"
+            elif int_grade in range(70,73):
+                return "B-"
+            else:
+                return "FZ"
+
+        # assign the value to letter_grade
+        # hint: letter_grade = mark_to_letter(grade)
+        letter_grade = mark_to_letter(grade)
+        print(letter_grade)
+
+    else:
+        # raise a TypeError exception
+        raise TypeError("Invalid type passed as parameter")
+
+    # write a long if-statement to convert letter_grade
+    if letter_grade == "A+":
+        gpa = 4.0
+    elif letter_grade == "A":
+        gpa = 4.0
+    elif letter_grade == "A-":
+        gpa = 3.7
+    elif letter_grade == "B+":
+        gpa = 3.3
+    elif letter_grade == "B":
+        gpa = 3.0
+    elif letter_grade == "B-":
+        gpa = 2.7
+    else:
+        gpa = 0.0
+   # assign the value to gpa
+
+    return gpa
--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,35 @@
-git reset --hard HEAD
+#!/usr/bin/env python3
+# Does collaboration work?
+#lets see if it works
+
+def decide_rps(player1, player2):
+    '''
+    Rock Paper Scissors Program.
+    Gets Both player's selection, ocmpare and return either
+    0 for tie, 1 for player 1 win, 2 for player 2 win.
+    :param player1:String Either Rock, Paper, or Scissors
+    :param player2:String Either Rock, Paper, or Scissors 
+    :return:Integer, either 0, 1, or 2
+
+    '''
+
+    #Dictionary for RPS Results
+    rps_results = {}
+    rps_results[("Rock", "Paper")] = 2
+    rps_results[("Rock", "Scissors")] = 1
+    rps_results[("Paper", "Rock")] = 1
+    rps_results[("Paper", "Scissors")] = 2
+    rps_results[("Scissors", "Paper")] = 1
+    rps_results[("Scissors", "Rock")] = 2
+
+    #check if input is both string
+    if type(player1) and type(player2) ==str:
+        #check if input string is either "Rock, Paper, or Scissors"
+        if (player1, player2) in rps_results:
+            return rps_results.get((player1, player2))
+        elif player1 == player2:
+            return 0
+        else:
+            raise ValueError ("Not Rock, Paper, Scissors")
+    else:
+        raise TypeError ("Not String")
