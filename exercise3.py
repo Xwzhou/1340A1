b540c36c264f7120df1e633bc094880bd024494c
Xwzhou <Xw.zhou@mail.utoronto.ca>
2014-10-06 22:48:10 -0400

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -10,39 +10,32 @@ def decide_rps(player1, player2):
     :result is a list for pairs regarding on winning decision
 
     """
-    signs = ["rock", "paper", "scissors"]
+    signs = ["Rock", "Paper", "Scissors"]
 
-    player1 = input("Hi,player 1,please input your sign")
-    player2 = input("Hi,player 2,please input your sign")
+    #catergorizing results by adding up strings
+    result = player1 + player2
+    player1_win=["RockScissors","ScissorsPaper","PaperRock"]
+    player2_win=["RockPaper","ScissorsRock","PaperScissors"]
+    tie=["RockRock","ScissorsScissors","PaperPaper"]
 
     # check input type and value
-    if type(player1) == str:
-        if player1 in signs:
-            if type(player2) == str:
-                if player2 in signs:
-
-
-                else:
-                     raise ValueError("the value of input is not valid")
-            else:
-                raise TypeError("the type of input is not valid")
+    if type(player1) == str and type(player2) == str:
+        if player1 in signs and player2 in signs:
+            if result in player1_win:
+                return 1
+            elif result in player2_win:
+                return 2
+            elif result in tie:
+                return 0
         else:
             raise ValueError("the value of input is not valid")
+         #decision making
+
     else:
         raise TypeError("the type of input is not valid")
 
 
     # get input into pair
 
-    result = player1 + player2
-    player1_win=["rockscissors","scissorspaper","paperrock"]
-    player2_win=["rockpaper","scissorsrock","paperscissors"]
-    tie=["rockrock","scissorsscissors","paperpaper"]
 
-    #decision making
-    if result in player1_win:
-        return 1
-    elif result in player2_win:
-        return 2
-    elif result in tie:
-        return 0
+
